5a98a70e962a34a4f8a362554312757f06644c45
3lackC4t <CScottDev01@proton.me>
2025-02-26 05:42:33 +0000

    Echo server works for single connection

--- 0_Smoke_Signal/src/echo.c
+++ 0_Smoke_Signal/src/echo.c
@@ -2,7 +2,6 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -10,49 +9,62 @@
 #define BUFFER_SIZE 32
 #define PORT 9999
 
-void handleCommunication() {
-  // Will handle incoming communications
+void handleCommunication(int client_fd) {
+  char buffer[BUFFER_SIZE];
+  ssize_t bytes_recieved;
+
+  while ((bytes_recieved = recv(client_fd, buffer, BUFFER_SIZE - 1, 0))) {
+    buffer[bytes_recieved] = '\0';
+    printf("Received %s\n", buffer);
+
+    send(client_fd, buffer, bytes_recieved, 0);
+  }
+}
+
+void handleError(const char *message, int socket_fd, int exitingFlag) {
+  perror(message);
+
+  if (socket_fd >= 0 && exitingFlag == 1) {
+    close(socket_fd);
+  }
+
+  exit(EXIT_FAILURE);
 }
 
 void echoServer() {
-  int server_fd, bind_fd, listen_fd;
-  struct sockaddr_in server_addr;
-  socklen_t addrlen = sizeof(server_addr);
-  char buffer[BUFFER_SIZE] = {0};
+  int server_fd, client_fd;
+  struct sockaddr_in server_addr, client_addr;
+  socklen_t addrlen = sizeof(client_addr);
 
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
   if (server_fd < 0) {
-    perror("Socket Failed");
-    exit(EXIT_FAILURE);
+    handleError("Socket Failure", server_fd, 1);
   }
 
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(PORT);
 
-  bind_fd = bind(server_fd, (struct sockaddr *)&server_addr,
-                 sizeof(struct sockaddr_in));
-
-  if (bind_fd < 0) {
-    perror("Bind Failur");
-    exit(EXIT_FAILURE);
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr))) {
+    handleError("Socket Bind Failuer", server_fd, 1);
   }
 
-  listen_fd = listen(server_fd, 10);
-
-  if (listen_fd < 0) {
-    perror("Listen Failure");
-    exit(EXIT_FAILURE);
+  if (listen(server_fd, 10) < 0) {
+    handleError("Socket Listen Failuer", server_fd, 1);
   }
 
   for (;;) {
-    accept_fd = accept(server_fd, (struct sockaddr *)&server_addr, addrlen);
+    client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addrlen);
 
-    if (accept_fd == -1) {
-      perror("Accept Failure");
-      exit(EXIT_FAILURE);
+    if (client_fd < 0) {
+      handleError("Server accept failure", client_fd, 0);
     }
+
+    handleCommunication(client_fd);
   }
+
+  close(server_fd);
 }
 
 int main(int argc, char const *argv[]) {
